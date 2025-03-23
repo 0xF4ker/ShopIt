1e4f77579ab1a68de79e04db5edbedd076f211de
Divine Attah-Ohiemi <ohiemidivine7@gmail.com>
2025-03-23 15:36:04 +0100

    modified mongo env url

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -3,22 +3,20 @@ import products from "./data.js";
 import Product from "../models/product.js";
 
 const seedProducts = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
-    );
+	try {
+		await mongoose.connect(process.env.DB_URI);
 
-    await Product.deleteMany();
-    console.log("Products are deleted");
+		await Product.deleteMany();
+		console.log("Products are deleted");
 
-    await Product.insertMany(products);
-    console.log("Products are added");
+		await Product.insertMany(products);
+		console.log("Products are added");
 
-    process.exit();
-  } catch (error) {
-    console.log(error.message);
-    process.exit();
-  }
+		process.exit();
+	} catch (error) {
+		console.log(error.message);
+		process.exit();
+	}
 };
 
 seedProducts();
