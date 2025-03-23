d7bbb588729bd3d4571931f17ad5534f41920aa9
Divine Attah-Ohiemi <ohiemidivine7@gmail.com>
2025-03-23 16:16:46 +0100

    added signature

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="py-1 pt-5">
-      <p className="text-center mt-1 fw-bold">
-        ShopIT - 2020-2023, All Rights Reserved
-      </p>
-    </footer>
-  );
+	return (
+		<footer className="py-1 pt-5">
+			<p className="text-center mt-1 fw-bold">
+				ShopIT - 2020-2025, All Rights Reserved, By 0xFaker
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
